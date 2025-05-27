67f4a6baa8850b00a19f5c88b4ba8971c388942e
MigueM531 <MigueM531>
2025-05-27 12:16:02 -0500

    primer blueprint insertar

--- src/view/web/plano.py
+++ src/view/web/plano.py
@@ -5,6 +5,7 @@ blueprint = Blueprint( "plano", __name__, "templates" )
 import sys
 sys.path.append("src")
 from model.Usuario import Usuario
+from model.tarjeta import Tarjeta
 from src.controller.tarjetas_controller import TarjetasController
 
 
@@ -16,10 +17,26 @@ def hola():
 def buscar():
     return render_template('buscar.html')
 
-@blueprint.route('/lista_tarjetas')
+@blueprint.route('/tarjetas_encontradas')
 def lista_tarjetas():
     resultado = TarjetasController.BuscarPorCedula(request.args["cedula"])
-    return render_template('lista_tarjetas.html', cedula=request.args["cedula"], tarjetas=resultado  )
+    return render_template('tarjetas_encontradas.html', cedula=request.args["cedula"], tarjetas=resultado  )
+
+@blueprint.route('/insertar')
+def insertar():
+    tarjeta_insertada = TarjetasController.Insertar(Tarjeta(
+        numero_tarjeta = request.args["numero_tarjeta"],
+        cedula = request.args["cedula"],
+        franquicia = request.args["franquicia"],
+        codigo_banco = request.args["codigo_banco"],
+        fecha_vencimiento = request.args["fecha_vencimiento"],
+        cupo = float(request.args["cupo"]),
+        tasa_interes = float(request.args["tasa_interes"]),
+        cuota_manejo = float(request.args["cuota_manejo"])
+    ))
+    
+    return render_template('insertar.html', tarjeta_insertada=tarjeta_insertada)
+
 
 if __name__ == "__main__":
     blueprint.run(debug=True)
