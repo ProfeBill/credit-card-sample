7d4ff68148ec08f7e47ef39c02a57cfdd4086ff8
MigueM531 <MigueM531>
2025-05-27 22:39:16 -0500

    importaciones de carpetas

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask import render_template, request
 import sys
 
 sys.path.append("src")
-from src.controller.tarjetas_controller import TarjetasController
 from src.view.web import plano
+from src.controller.tarjetas_controller import TarjetasController
 
 app = Flask(__name__)
+
 
 app.register_blueprint( plano.blueprint )
 
